a0daeb5f2885c1b39853342e5a9797f900da3409
Mrinalini Upadhya <MrinaliniU@users.noreply.github.com>
2022-04-10 23:55:48 -0700

    tests added

--- src/main/java/com/example/my_project/mapper/ParkingSpotMapper.java
+++ src/main/java/com/example/my_project/mapper/ParkingSpotMapper.java
@@ -3,7 +3,6 @@ package com.example.my_project.mapper;
 import com.example.my_project.dtos.ParkingSpotDTO;
 import com.example.my_project.model.ParkingSpot;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
